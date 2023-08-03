e3ebe52c0480bea6689339c258f52b8326064142
walyson <walysonmoises1342@gmail.com>
2023-08-02 23:43:31 -0300

    print hello word

---
+++ src/main/java/com/waly/hellospringbatch/step/PrintHelloStepConfig.java
@@ -0,0 +1,22 @@
+package com.waly.hellospringbatch.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class PrintHelloStepConfig {
+
+    @Bean
+    public Step printHelloStep(JobRepository jobRepository, PlatformTransactionManager transactionalManager, Tasklet printHelloTasklet){
+        return new StepBuilder("printHelloStep", jobRepository)
+                .tasklet(printHelloTasklet, transactionalManager).build();
+    }
+}
